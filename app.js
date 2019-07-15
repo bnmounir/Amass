4ba637ec38a156840505e1d4a5e82289ebc0981c
Mounir Benabderrahmane <bnmounir7@gmail.com>
2019-07-15 14:46:07 -0400

    add interval get and new name amass

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 const express = require('express'),
-  app = express(),
-  bodyParser = require('body-parser'),
-  mongoose = require('mongoose'),
-  session = require('express-session'),
-  flash = require('connect-flash'),
-  passport = require('passport'),
-  LocalStrategy = require('passport-local'),
-  methodOverride = require('method-override'),
-  User = require('./models/user');
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    session = require('express-session'),
+    flash = require('connect-flash'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    methodOverride = require('method-override'),
+    User = require('./models/user');
 require('dotenv').config();
 
 const indexRoutes = require('./routes/index'),
-  subjectsRoutes = require('./routes/subjects'),
-  commentRoutes = require('./routes/comments');
+    subjectsRoutes = require('./routes/subjects'),
+    commentRoutes = require('./routes/comments');
 //for local work "mongodb://localhost:27017/local_db" ||
 //for deployed app process.env.DATABASEURL
 const url = process.env.DATABASEURL;
 const port = process.env.PORT || 3000;
-mongoose.connect(
-  url,
-  { useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -31,11 +28,11 @@ app.locals.moment = require('moment');
 
 //passport config
 app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false
-  })
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -44,16 +41,21 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
-  res.locals.currentUser = req.user;
-  res.locals.error = req.flash('error');
-  res.locals.success = req.flash('success');
-  next();
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
+    next();
 });
 
 app.use('/', indexRoutes);
 app.use('/subjects', subjectsRoutes);
 app.use('/subjects/:id/comments', commentRoutes);
 
+var http = require('http');
+setInterval(function() {
+    http.get('https://limitless-lake-16790.herokuapp.com');
+}, 300000);
+
 app.listen(port, process.env.IP, () => {
-  console.log(`Server Started: Talktome website on port ${port}!`);
+    console.log(`Server Started: Amass website on port ${port}!`);
 });
